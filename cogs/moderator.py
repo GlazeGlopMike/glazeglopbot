1079233f1b044ec14ee88664f1b56d2a51831272
Glaze Glop <glazeglopmike@gmail.com>
2021-06-18 21:20:15 -0400

    Added user video blackout feature

--- cogs/moderator.py
+++ cogs/moderator.py
@@ -25,14 +25,61 @@ class Moderator(commands.Cog):
                 msg += f'{member.name}, '
             msg += f'and {members[-1].name}'
         return msg
+
+    @commands.command(aliases=['bo', 'sv', 'stopvid', 'stopvideo'])
+    async def blackout(self, ctx):
+        """
+        Ends tagged users' streams by moving them to a temporary channel, then  
+        moving them back.
+
+        Requires Move Members permission.
+        """
+        # verify permissions
+        if not ctx.author.guild_permissions.move_members:
+            await ctx.message.add_reaction('\U0001F44E');
+            await ctx.send("You lack this authority!")
+            return
+        
+        mentions = ctx.message.mentions
+        temp_channel = await ctx.guild.create_voice_channel('BLACKOUT')
+        
+        if mentions:
+            skipped = []
+            
+            for user in mentions:
+                voice = user.voice
+
+                if not (voice and voice.channel):
+                    skipped.append(user)
+                    continue
+                
+                try:
+                    channel = voice.channel
+                    await user.move_to(temp_channel)
+                    await user.move_to(channel)
+                except discord.errors.HTTPException:
+                    skipped.append(user)
+
+            if not skipped:
+                await ctx.send("All video streams successfully stopped.")
+            elif len(skipped) == len(mentions):
+                await ctx.message.add_reaction('\U0001F615');
+                await ctx.send("No video streams were stopped.")
+            else:
+                await ctx.message.add_reaction('\u26A0');
+                await ctx.send(f"Some video stream(s) successfully moved. "
+                               f"{self.skipped_msg(ctx, skipped)}")
+        else:
+            await ctx.message.add_reaction('\U0001F615');
+            await ctx.send(f"No users specified.")
+
+        await temp_channel.delete()
     
     @commands.command(aliases=['dc'])
     async def disconnect(self, ctx):
         """
-        Disconnects users from their voice channels by moving them to a
+        Disconnects tagged users from their voice channels by moving them to a 
         temporary channel, then deleting that channel.
-        
-        Attempts to move tagged users, or the command author if none named.
 
         Requires Move Members permission.
         """
@@ -74,10 +121,10 @@ class Moderator(commands.Cog):
         
         await temp_channel.delete()
 
-    @commands.command()
+    @commands.command(aliases=['drag'])
     async def summon(self, ctx):
         """
-        Moves users to author's voice channel
+        Moves users to author's voice channel.
 
         Requires Move Members permission.
         """
