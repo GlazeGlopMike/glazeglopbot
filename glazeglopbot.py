24619eff5772f7471d68ba99fbcab7286b435bae
Glaze Glop <glazeglopmike@gmail.com>
2021-07-12 22:00:07 -0400

    Changed import order

--- glazeglopbot.py
+++ glazeglopbot.py
@@ -1,9 +1,10 @@
 # glazeglopbot.py
 import os
-
 import discord
-from discord.ext import commands
 import dotenv
+import discord
+
+from discord.ext import commands
 
 # setting up client
 dotenv.load_dotenv()
